25349041b543cb8e8abf0bf800c47e25c1a36e19
Imbibit <905938168@qq.com>
2024-10-24 12:57:03 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php  
+	//echo"旧衣物改造升级系统";
+?>  
+<html>
+	<?php
+		for($i=0;$i<=15;$i++) echo"<br>" 
+	?>
+	<table width="400" border="2"  style="border:1px solid black;margin-left:auto;margin-right:auto;">
+		<?php
+				for($i=0;$i<=15;$i++) echo"&nbsp;" 
+		?>
+		<tr>
+			<th>用户名</th>
+			<th>
+			<?php
+				for($i=0;$i<=15;$i++) echo"&nbsp;" 
+			?>
+			</th>
+		</tr>
+		<tr>
+			<th>密码</th>
+			<th></th>
+		</tr>
+	</table>
+</html>
